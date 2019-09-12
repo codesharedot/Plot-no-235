3b6c164f2b2623cec607d4e2f821f2ce849a4edf
codesharedot <codeto@sent.com>
2019-09-12 20:38:28 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Line Chart Haydee 
+# Line Chart Tanya 
 
 Chart example for Python
 
--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-1, 1, 50)
-y = 1*x
+y = 8*x
 
-plt.plot(x, y,'r-',linewidth=7)
+plt.plot(x, y,'bo',linewidth=3)
 plt.savefig('chart.png')
