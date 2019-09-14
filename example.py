4e291342e1f936b16b2eb8807b63bac465e84df8
codesharedot <codeto@sent.com>
2019-09-14 17:30:42 +0200

    first commit

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-1, 1, 50)
-y = 3*x*x
+y = 2*x*x
 
-plt.plot(x, y,'y:',linewidth=2)
+plt.plot(x, y,'b-.',linewidth=4)
 plt.savefig('chart.png')
