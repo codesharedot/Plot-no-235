ca907d6029e10cf954411fd2c4944ac70f9619b6
codesharedot <codeto@sent.com>
2019-09-15 20:14:06 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Quadratic Line Chart Karen 
+# Quadratic Line Chart Nellie 
 
 Example for my book (matplotlib, python)
 
--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-1, 1, 50)
-y = 7*x*x
+y = 2*x*x
 
-plt.plot(x, y,'m-',linewidth=4)
+plt.plot(x, y,'y-',linewidth=1)
 plt.savefig('chart.png')
