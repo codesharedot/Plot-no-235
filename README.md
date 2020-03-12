44d42f32efdd5000c1e21859327d21d193e561fa
compilerboiler <codeto@sent.com>
2020-03-12 13:23:26 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Plot no 127 
+# Plot no 128 
 
 matplotlib example
 
--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(-1, 1, 50)
-y = 3*x
+y = 4*x
 
-plt.plot(x, y,'b-.',linewidth=8)
+plt.plot(x, y,'m-.',linewidth=2)
 plt.savefig('chart.png')
